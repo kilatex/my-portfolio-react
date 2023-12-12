ddd999ef1e893d0d2253bf4e5a48379f170b11b5
santiago <santiagodmaldon@gmail.com>
2023-12-11 23:53:33 -0600

    nav links

--- src/components/layouts/HeaderComponent.jsx
+++ src/components/layouts/HeaderComponent.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export const HeaderComponent = () => {
   return (
     
     <header>
-        <h1>Header Component</h1>
+      <div className="logo">
+        <span>S</span>
+      <h3>santiagodsan</h3>
+      </div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/home">Home</NavLink>
+              <NavLink to="/portfolio">Portfolio</NavLink>
+              <NavLink to="/services">Services</NavLink>
+              <NavLink to="/cv">My CV</NavLink>
+              <NavLink to="/contact ">Contact</NavLink>
+
+            </li>
+          </ul>
+        </nav>
     </header>
   )
 }

80f196d0504d5ee6b3bb4bc5caae2a20d36b8f9c
santiago <santiagodmaldon@gmail.com>
2023-12-14 08:01:33 -0600

    portfolio section

---
+++ src/assets/myProjects.js
@@ -0,0 +1,58 @@
+export const myProjects = [
+    {
+      'id': 'facebookid',
+      'name': 'Facebook',
+      'url': 'www.facebook.com',
+      'technologies': 'HTML, CSS, JS, PHP, MySQL',
+      'category': 'Social Media Platform'
+    },
+    {
+      'id': 'twitterid',
+      'name': 'Twitter',
+      'url': 'www.twitter.com',
+      'technologies': 'HTML, CSS, JS, Node.js, MongoDB',
+      'category': 'Social Media Platform'
+    },
+    {
+      'id': 'instagramid',
+      'name': 'Instagram',
+      'url': 'www.instagram.com',
+      'technologies': 'ReactJS, Node.js, Express, MongoDB',
+      'category': 'Social Media Platform'
+    },
+    {
+      'id': 'blogid',
+      'name': 'Personal Blog',
+      'url': 'www.myblog.com',
+      'technologies': 'Gatsby, GraphQL, Markdown',
+      'category': 'Blogging Platform'
+    },
+    {
+      'id': 'ecommerceid',
+      'name': 'E-commerce Platform',
+      'url': 'www.ecommerce.com',
+      'technologies': 'ReactJS, Redux, Firebase',
+      'category': 'E-commerce'
+    },
+    {
+      'id': 'tasktrackerid',
+      'name': 'Task Tracker App',
+      'url': 'www.tasktracker.com',
+      'technologies': 'Vue.js, Vuex, JSON Server',
+      'category': 'Productivity Tool'
+    },
+    {
+      'id': 'portfolio',
+      'name': 'Portfolio Website',
+      'url': 'www.yourportfolio.com',
+      'technologies': 'ReactJS, HTML, CSS',
+      'category': 'Personal Portfolio'
+    },
+    {
+      'id': 'mobileapp',
+      'name': 'Mobile App Prototype',
+      'url': 'www.mobileapp.com',
+      'technologies': 'React Native, Firebase',
+      'category': 'Mobile Application'
+    },
+  ];
--- src/components/PortfolioComponent.jsx
+++ src/components/PortfolioComponent.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { myProjects } from '../assets/myProjects';
 
 export const PortfolioComponent = () => {
   return (
-    <div>PortfolioComponent</div>
-  )
-}
+    <div className="portfolio-container">
+      <h2>My Portfolio</h2>
+      <div className="projects">
+        {myProjects.map((project) => (
+          <div key={project.id} className="project-card">
+            <div className='projects-img'>
+              <img src={"/img/"+project.id+".png"} alt="" />
+            </div>
+            <h3>{project.name}</h3>
+            <p><strong>Category:</strong> {project.category}</p>
+            <p><strong>Technologies:</strong> {project.technologies}</p>
+            <p><strong>URL:</strong> <a href={project.url} target="_blank" rel="noopener noreferrer">{project.url}</a></p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
